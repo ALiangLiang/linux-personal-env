d77a26c84db0aa8071a7d097b73f34d8aa1c55f8
ALiangLiang <me@aliangliang.top>
2018-09-29 06:05:48 +0800

    Install vundle

--- setup-vim-env.sh
+++ setup-vim-env.sh
@@ -1,4 +1,5 @@
 cd ~
 which vim && echo "Already install vim." || (echo "Doesn't has vim. Please install vim."; exit 0)
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp linux-personal-env/.vimrc .vimrc
